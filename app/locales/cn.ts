cc6ff3aa858d3a7d2b73cd29b4bcf3c56cca95b9
Mchousexuxu <133076068+Mchousexuxu@users.noreply.github.com>
2023-08-31 06:42:42 +0800

    Update cn.ts
    text commit

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -266,7 +266,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
+    BotHello: "欢迎使用Nippon GPT工具,您可以直接向我提问",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
